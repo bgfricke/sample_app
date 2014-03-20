db89c959b00be0b0bc0b92b57db5bf7df125fce2
Bryan Fricke <bryan.fricke@gmail.com>
2014-03-20 15:24:36 -0700

    Partial User Authentication 3/20/2014:18:30

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
  	def show
-    	@user = User.find(params[:id])
+    @user = User.find(params[:id])
 	end
 
 	def new
@@ -11,17 +11,17 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+      #sign_in @user
 			flash[:success] = "Welcome to the Sample App!"
-      		redirect_to @user
-    	else
-      		render 'new'
-      	end
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
-
-  	private
-
-  		def user_params
-  			params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  		end
-
 end
