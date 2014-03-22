62fcbc1ecd6e541776967804c47930ef687e081a
Bryan Fricke <bryan.fricke@gmail.com>
2014-03-22 13:43:39 -0700

    Partial ch 9:03/22/2014:17:00

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user,  only: [:index, :edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
+
+  def index
+  end
 
  	def show
     @user = User.find(params[:id])
@@ -20,11 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -38,4 +41,18 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,8 +33,8 @@ describe "User pages" do
       before do
         fill_in "Name", with: "Example User"
         fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Password", with: "foobar23"
+        fill_in "Confirmation", with: "foobar23"
       end
 
       it "should create a user" do
